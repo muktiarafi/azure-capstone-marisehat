626c439be1a256c9d6459631443281a9337ca6c9
muktiarafi <muktiarafi@gmail.com>
2021-11-28 21:42:14 +0700

    configure security config

--- marisehat/src/main/java/dev/muktiarafi/marisehat/config/SecurityConfig.java
+++ marisehat/src/main/java/dev/muktiarafi/marisehat/config/SecurityConfig.java
@@ -1,23 +1,35 @@
 package dev.muktiarafi.marisehat.config;
 
 import com.azure.spring.aad.webapp.AADWebSecurityConfigurerAdapter;
+import com.azure.spring.autoconfigure.aad.AADAppRoleStatelessAuthenticationFilter;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@AllArgsConstructor
 public class SecurityConfig extends AADWebSecurityConfigurerAdapter {
+    private final AADAppRoleStatelessAuthenticationFilter filter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
         http.authorizeRequests()
                 .mvcMatchers(HttpMethod.POST, "/users").permitAll()
-                .mvcMatchers("/admins/**").hasAuthority("APPROLE_Admin")
+                .mvcMatchers("/admins/**").hasRole("Admin")
+                .mvcMatchers(HttpMethod.GET, "/patients/**").hasAnyRole("Admin", "User", "Partner")
+                .mvcMatchers(HttpMethod.POST, "/patients/**").hasAnyRole("Admin", "User")
+                .mvcMatchers(HttpMethod.PUT, "/patients/**").hasAnyRole("Admin", "User")
+                .mvcMatchers(HttpMethod.DELETE, "/patients/**").hasAnyRole("Admin", "User")
                 .anyRequest().authenticated();
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
